37d76f0e48b69f1575843b4e453830cc8c93d594
Darshan Sawardekar <darshan@sawardekar.org>
2014-05-16 18:12:02 +0530

    Upgrades to Arrow 0.7.0

--- wp-syntax-highlighter.php
+++ wp-syntax-highlighter.php
@@ -9,29 +9,15 @@ Plugin URI: http://wordpress.org/plugins/wp-syntax-highlighter
 License: GPLv2
 */
 
-require_once(__DIR__ . '/vendor/dsawardekar/wp-requirements/lib/Requirements.php');
+require_once(__DIR__ . '/vendor/dsawardekar/arrow/lib/Arrow/ArrowPluginLoader.php');
 
 function wp_syntax_highlighter_main() {
-  $requirements = new WP_Requirements();
+  $options = array(
+    'plugin' => 'WpSyntaxHighlighter\Plugin',
+    'arrowVersion' => '0.7.0'
+  );
 
-  if ($requirements->satisfied()) {
-    wp_syntax_highlighter_register();
-  } else {
-    $plugin = new WP_Faux_Plugin('WP Syntax Highlighter', $requirements->getResults());
-    $plugin->activate(__FILE__);
-  }
-}
-
-function wp_syntax_highlighter_register() {
-  require_once(__DIR__ . '/vendor/dsawardekar/arrow/lib/Arrow/ArrowPluginLoader.php');
-
-  $loader = ArrowPluginLoader::getInstance();
-  $loader->register(__FILE__, '0.5.1', 'wp_syntax_highlighter_load');
-}
-
-function wp_syntax_highlighter_load() {
-  $plugin = \WpSyntaxHighlighter\Plugin::create(__FILE__);
-  $plugin->enable();
+  ArrowPluginLoader::load(__FILE__, $options);
 }
 
 wp_syntax_highlighter_main();
